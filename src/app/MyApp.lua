2ae69258367a188348cbb9761a1c41c44b902e9f
DoooReyn <jl88744653@gmail.com>
2017-12-10 17:47:34 +0800

    mod rename

--- src/app/MyApp.lua
+++ src/app/MyApp.lua
@@ -37,11 +37,11 @@ end
 
 function MyApp:loadModules()
     Game.Modules = {}
-    require('app.module.ResMgr').new()
-    require('app.module.SrcMgr').new()
-    require('app.module.DataMgr').new()
-    require('app.module.RenderMgr').new()
-    require('app.module.ViewMgr').new()
+    require('app.module.ResMod').new()
+    require('app.module.SrcMod').new()
+    require('app.module.DataMod').new()
+    require('app.module.RenderMod').new()
+    require('app.module.ViewMod').new()
 end
 
 function MyApp:loadAppVars()
