69ba558d2ab1e58202e51b31054d76748f121b7d
DoooReyn <jl88744653@gmail.com>
2017-12-11 06:33:17 +0800

    cache

---
+++ src/app/data/const/ViewConst.lua
@@ -0,0 +1,18 @@
+------------------------------------------------------------------------------------------
+---- Name   : ViewConst
+---- Desc   : 视图常量
+---- Date   : 2017/12/10
+---- Author : Reyn - jl88744653@gmail.com
+------------------------------------------------------------------------------------------
+
+----------------------------------------------------
+-- @param swallow : 吞噬触摸
+-- @param keep    : 是否常驻
+--
+return {
+    LoginView               = { swallow = true, keep = false },
+    CreateRoleView          = { swallow = true, keep = false },
+    MainScreenView          = { swallow = true, keep = true  },
+    MainMenuView            = { swallow = true, keep = true  },
+    SystemConfiguraionView  = { swallow = true, keep = false },
+}
