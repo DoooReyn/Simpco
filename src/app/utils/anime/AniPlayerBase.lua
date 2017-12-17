08118de457886a5d38476cebe321445690b55ae6
DoooReyn <jl88744653@gmail.com>
2017-12-18 01:42:57 +0800

    animation player

---
+++ src/app/utils/anime/AniPlayerBase.lua
@@ -0,0 +1,27 @@
+local AniPlayerBase = class('AniPlayerBase', cc.Node)
+
+function AniPlayerBase:ctor()
+
+end
+
+function AniPlayerBase:play()
+
+end
+
+function AniPlayerBase:stop()
+
+end
+
+function AniPlayerBase:pause()
+
+end
+
+function AniPlayerBase:cache()
+
+end
+
+function AniPlayerBase:uncache()
+
+end
+
+return AniPlayerBase
