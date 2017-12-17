6d22db5d14fd5ad66f33a977f7576f22cf143ae3
DoooReyn <jl88744653@gmail.com>
2017-12-18 02:21:24 +0800

    添加多语言切换支持
    TODO

--- src/app/core/LocaleCore.lua
+++ src/app/core/LocaleCore.lua
@@ -29,4 +29,33 @@ function LocaleCore:unload()
 
 end
 
+---------------------------------------------------------
+-- @desc: 核心卸载
+-- 
+function LocaleCore:localeAt()
+
+end
+
+---------------------------------------------------------
+-- @desc: 获得多语言对应文本
+-- @param : textId - 文本ID
+--
+function LocaleCore:getLocaleText(textId)
+
+end
+
+---------------------------------------------------------
+-- @desc: 获得多语言对应字体
+-- 
+function LocaleCore:getLocaleFont()
+
+end
+
+---------------------------------------------------------
+-- @desc: 重新载入多语言环境
+-- 
+function LocaleCore:reloadLocaleEnv()
+
+end
+
 return LocaleCore
